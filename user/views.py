78281fefe61ab8e0656a13db4db70c2b9b28543c
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-22 12:25:09 +0900

    Feat 기본설정
    1. settings.py : 앱추가 | REST_FRAMEWORK 추가 | 언어,시간 변경 | AUTH_USER_MODEL 설정
    2. urls- views 초기 설정 : UserView, ProductView 생성

--- user/views.py
+++ user/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework import permissions, status
+from rest_framework.response import Response
 
-# Create your views here.
+
+
+class UserView(APIView):
+    
+    def get(self, request):
+        return Response({"message": "get 성공"})
+    def post(self, request):
+        return Response({"message": "post 성공"})
+    def put(self, request):
+        return Response({"message": "put 성공"})
+    def delete(self, request):
+        return Response({"message": "delete 성공"})
+    
